5aab0b682a9128bf37f71bad4423ebb95e28acb9
Diego Rojas <diego@brasa.art.br>
2015-09-10 23:03:20 -0300

    implementadas imagens, rodape

--- footer.php
+++ footer.php
@@ -11,10 +11,18 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-logos">
+		</div>
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'intergalactic' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'intergalactic' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'intergalactic' ), 'Intergalactic', '<a href="http://wordpress.com/themes" rel="designer">Automattic</a>' ); ?>
+			<div class="esquerda">
+			<a>Este projeto foi contemplado pelo Prêmio Funarte Caixa Carequinha de Estímulo ao Circo 2014 e pela 1ª Edição de Fomento ao Circo para a Cidade de São Paulo</a>
+			</div>
+
+			<div class="direita">
+			<a href="<?php echo esc_url( __( 'http://br.wordpress.org/', 'intergalactic' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'intergalactic' ), 'WordPress' ); ?></a>
+			<br />
+			<?php printf( __( 'Theme: %1$s by %2$s', 'intergalactic' ), '<a href="https://wordpress.org/themes/intergalactic/" rel="designer">Intergalactic</a>', 'Automattic' ); ?>
+			</div>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
